11948eb19a4b3caef80ed660f20e0e4b948c5053
Kamal Ayman <52112682+kamal159@users.noreply.github.com>
2022-07-04 12:57:44 +0200

    Update post_tweet.py

--- post_tweet.py
+++ post_tweet.py
@@ -13,7 +13,7 @@ sleep(7)
 while 1:
     driver.find_element(by=By.XPATH, value="//a[@data-testid='SideNav_NewTweet_Button']").click()
     sleep(.1)
-    driver.find_element(by=By.XPATH, value="//div[@role='textbox']").send_keys(f"_{i} #انترنت_غير_محدود_في_مصر {i}")
+    driver.find_element(by=By.XPATH, value="//div[@role='textbox']").send_keys(f"_{i} #انترنت_غير_محدود_في_مصر {i}\n #Unlimated_Internet_In_Egypt {i}")
     driver.find_element(by=By.XPATH, value="//div[@data-testid='tweetButton']").click()
     i += 1
     sleep(.5)
